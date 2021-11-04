67a14b04a08ba49ca155d60053e73f3610d2a5c2
dwajsman <danielwajsman@mac.com>
2021-11-04 01:20:07 -0300

    first commit

---
+++ client.js
@@ -0,0 +1,64 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+ // MY CODE
+
+  conn.on("data", (data) => {
+    console.log(data);
+    conn.end();
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: DAN");
+   // conn.write("Move: up");
+
+ 
+
+
+
+  });
+
+
+  // conn.on('connect', () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 1000);
+  // });
+
+  // conn.on('connect', () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: left");
+  //   }, 2000);
+  // });
+
+
+  // conn.on('connect', () => {
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 500);
+  // });
+
+
+
+
+  return conn;
+};
+
+
+
+//connect();
+
+
+module.exports = {
+  connect,
+}
